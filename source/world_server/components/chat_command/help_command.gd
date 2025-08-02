0a841416858700eb335f7da93e5b6c3d9ae3bb5e
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-08-02 16:03:22 -0400

    Update help_command.gd

--- source/world_server/components/chat_command/help_command.gd
+++ source/world_server/components/chat_command/help_command.gd
@@ -3,11 +3,10 @@ extends ChatCommand
 
 func execute(args: PackedStringArray, peer_id: int, server_instance: ServerInstance) -> String:
 	var command_list:Array = server_instance.chat_commands.keys()
-	var parsed_list: Array[String]
-	var list: String
+	var parsed_list: String
 	for command in command_list:
-		if not list:
-			list += str(command)
+		if not parsed_list:
+			parsed_list += str(command)
 		else:
-			list += "\n" + str(command)
-	return str(list)
+			parsed_list += "\n" + str(command)
+	return str(parsed_list)
