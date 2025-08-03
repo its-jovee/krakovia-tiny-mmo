19cac06ad03f066ef20bc683ab6047a6322e96f4
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-08-03 11:51:42 -0400

    Update help_command.gd
    Added suggestions from @SlayHorizon

--- source/world_server/components/chat_command/help_command.gd
+++ source/world_server/components/chat_command/help_command.gd
@@ -3,10 +3,7 @@ extends ChatCommand
 
 func execute(_args: PackedStringArray, _peer_id: int, server_instance: ServerInstance) -> String:
 	var command_list:Array = server_instance.chat_commands.keys()
-	var parsed_list: String = ""
-	for command in command_list:
-		if not parsed_list:
-			parsed_list += str(command)
-		else:
-			parsed_list += "\n" + str(command)
-	return str(parsed_list)
+	var parsed_list: String = "\n".join(command_list)
+	if not server_instance.local_chat_commands.is_empty():
+		parsed_list += "\n".join(server_instance.local_chat_commands.keys())
+	return parsed_list
