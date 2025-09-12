dd8964b7136e807b7730852a48e230a783558e37
SlayHorizon <163869998+SlayHorizon@users.noreply.github.com>
2025-09-12 14:25:33 +0200

    Start to rework the chat by adding full feed view

--- source/client/ui/chat/chat.gd
+++ source/client/ui/chat/chat.gd
@@ -1,39 +1,45 @@
 extends Control
 
 
+@export var closed_button: Button
+@export var full_feed_text: RichTextLabel
+
 var channel_messages: Dictionary[int, PackedStringArray]
 var current_channel: int = 0
+var fade_out_tween: Tween
+
+@onready var peek_feed: VBoxContainer = $PeekFeed
 
 
 func _ready() -> void:
+	peek_feed.show()
+	$FullFeed.hide()
 	Events.message_received.connect(_on_message_received)
 
 
+#func _unhandled_key_input(event: InputEvent) -> void:
 func _input(_event: InputEvent) -> void:
-	if (
-		Input.is_action_just_pressed("chat")
-		and not %MessageEdit.has_focus()
-	):
+	#if (
+		#Input.is_action_just_pressed("chat")
+		#and not %MessageEdit.has_focus()
+	#):
+	if _event.is_action_pressed(&"chat") and not %MessageEdit.has_focus():
+		get_viewport().set_input_as_handled()
 		accept_event()
 		open_chat()
 
 
 func open_chat() -> void:
-	%MessageDisplay.show()
-	%MessageEdit.show()
+	peek_feed.show()
+	reset_view()
 	%MessageEdit.grab_focus()
 	%FadeOutTimer.stop()
-
-
-func hide_chat() -> void:
-	%MessageDisplay.hide()
-	%MessageEdit.hide()
 
 
 func _on_message_submitted(new_message: String) -> void:
 	%MessageEdit.clear()
 	%MessageEdit.release_focus()
-	%MessageEdit.hide()
+	#%MessageEdit.hide()
 	if not new_message.is_empty():
 		new_message = new_message.strip_edges(true, true)
 		new_message = new_message.substr(0, 120)
@@ -55,17 +61,42 @@ func _on_message_received(message: String, sender_name: String, channel: int):
 	if channel_messages.has(channel):
 		channel_messages[channel].append(message_to_display)
 	else:
-		channel_messages[channel] = PackedStringArray()
+		channel_messages[channel] = PackedStringArray([message_to_display])
 
 
 func _on_fade_out_timer_timeout() -> void:
-	var tween := create_tween()
-	tween.tween_property(self, "modulate:a", 0, 0.3)
-	await tween.finished
-	hide_chat()
-	modulate.a = 1.0
+	if %MessageEdit.has_focus():
+		%FadeOutTimer.start()
+		return
+	
+	if fade_out_tween:
+		fade_out_tween.kill()
+	
+	fade_out_tween = create_tween()
+	fade_out_tween.tween_property(peek_feed, ^"modulate:a", 0, 0.3)
 
 
 func _on_peek_feed_gui_input(event: InputEvent) -> void:
+	print(event)
+	if event is InputEventMouseMotion and peek_feed.modulate.a < 1.0:
+		reset_view()
+		%FadeOutTimer.start()
+		return
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		$PeekFeed.hide()
+		peek_feed.hide()
+		$FullFeed.show()
+		full_feed_text.clear()
+		for message: String in channel_messages[0]:
+			full_feed_text.append_text(message)
+
+
+func _on_close_button_pressed() -> void:
+	peek_feed.show()
+	reset_view()
+	$FullFeed.hide()
+
+
+func reset_view() -> void:
+	if fade_out_tween and fade_out_tween.is_running():
+		fade_out_tween.kill()
+	peek_feed.modulate.a = 1.0
